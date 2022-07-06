b1ba3186496e706293e02d1de06793fe8c587e61
Patrick Sliwinski <patrick.sliwinski@gatech.edu>
2022-07-05 23:55:54 -0400

    Create session model

--- ProjectTracker/projects/models.py
+++ ProjectTracker/projects/models.py
@@ -1,10 +1,24 @@
+from wsgiref.validate import validator
 from django.conf import settings
+from django.core.validators import ValidationError, MaxLengthValidator
 from django.db import models
-
+from oauth.models import User
 # Create your models here.
 
 class Project(models.Model):
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    title = models.CharField(max_length=60)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=60, validators=[MaxLengthValidator(60)])
+
+class Session(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+
+    def clean(self):
+        if self.start_date > self.end_date:
+            raise ValidationError('Start date is after end date')
+
+    
+     
 
     
--- ProjectTracker/projects/tests.py
+++ ProjectTracker/projects/tests.py
@@ -1,9 +1,11 @@
 from django.db.utils import IntegrityError
+from django.core.validators import ValidationError
 from django.test import TestCase
 from django.utils import timezone
+import datetime
 
 
-from projects.models import Project
+from projects.models import Project, Session
 from oauth.models import User
 # Create your tests here.
 
@@ -26,3 +28,35 @@ class ProjectTests(TestCase):
         self.assertIn(project2, Project.objects.filter(owner=owner2))
         self.assertNotIn(project1, Project.objects.filter(owner=owner2))
         self.assertNotIn(project2, Project.objects.filter(owner=owner1))
+
+    def test_too_long_title(self):
+        owner = User.objects.create(username="long_title_owner", password="password")
+        title = "skdfjhbrenwsmkdfjhbednwmskdfgjhfbrdenmsdkfjhbrdeskdjfhbdnemkskdfjhnemwskdfjhr"
+        project = Project.objects.create(owner=owner, title=title)
+        with self.assertRaises(ValidationError):
+            project.full_clean()
+
+class SessionTests(TestCase):
+    def setUp(self):
+        self.owner1 = User.objects.create(username="session_owner1", password="password")
+        self.owner2 = User.objects.create(username="session_owner2", password="password")
+        self.project1 = Project.objects.create(owner=self.owner1, title="Project1") 
+        self.project2 = Project.objects.create(owner=self.owner2, title="Project2")
+        self.now = datetime.datetime.now()
+
+    def test_create_session(self):
+        session = Session.objects.create(project=self.project1, start_date=self.now, end_date=self.now + datetime.timedelta(days = 1))
+        self.assertIn(session, Session.objects.all())
+    
+    def test_proper_project(self):
+        session1 = Session.objects.create(project=self.project1, start_date=self.now, end_date=self.now + datetime.timedelta(days = 1))
+
+        self.assertIn(session1, Session.objects.filter(project=self.project1))
+        self.assertNotIn(session1, Session.objects.filter(project=self.project2))
+    
+    def test_start_after_end(self):
+        session1 = Session.objects.create(project=self.project1, start_date=self.now, end_date=self.now - datetime.timedelta(days = 1))
+        with self.assertRaises(ValidationError):
+            session1.full_clean()
+
+        
