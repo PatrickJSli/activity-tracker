b921c757e93901e3ee74c8b7fa8c1f28892517c5
Patrick Sliwinski <patrick.sliwinski@gatech.edu>
2022-07-07 19:18:59 -0400

    Check if user is authenticated

--- ProjectTracker/projects/views.py
+++ ProjectTracker/projects/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+from projects.models import Project
 
 # Create your views here.
+
+def dashboard(request):
+    if not request.user.is_authenticated:
+        return render(request, 'projects/dashboard_noauth.html')
+
+    projects = Project.objects.filter(owner=request.user)
+    return render(request, 'projects/dashboard.html', {'projects' : projects})
