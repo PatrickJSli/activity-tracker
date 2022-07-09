05472b8df3d1f634a62ae0f40826b1eefe0a2fb8
Patrick Sliwinski <patrick.sliwinski@gatech.edu>
2022-07-08 23:19:46 -0400

    create modelform for projects

--- ProjectTracker/projects/models.py
+++ ProjectTracker/projects/models.py
@@ -1,6 +1,6 @@
-from wsgiref.validate import validator
 from django.conf import settings
 from django.core.validators import ValidationError, MaxLengthValidator
+from django.forms import ModelForm
 from django.db import models
 from oauth.models import User
 # Create your models here.
@@ -8,6 +8,11 @@ from oauth.models import User
 class Project(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=60, validators=[MaxLengthValidator(60)])
+
+class ProjectForm(ModelForm):
+    class Meta:
+        model = Project
+        fields = ['title',]
 
 class Session(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
--- ProjectTracker/projects/views.py
+++ ProjectTracker/projects/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
-from projects.models import Project
+from projects.models import Project, ProjectForm
 
 # Create your views here.
 
@@ -9,5 +9,13 @@ def dashboard(request):
     if not request.user.is_authenticated:
         return render(request, 'projects/dashboard_noauth.html')
 
+    if request.method == 'POST':
+        project = Project(owner=request.user)
+        formset = ProjectForm(request.POST, instance=project)
+        if formset.is_valid():
+            formset.save()
+    else:
+        formset = ProjectForm()
+
     projects = Project.objects.filter(owner=request.user)
-    return render(request, 'projects/dashboard.html', {'projects' : projects})
+    return render(request, 'projects/dashboard.html', {'projects' : projects, 'formset' : formset})
